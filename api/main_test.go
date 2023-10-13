c37c4cc7ee811627194d1be4ef1919fc4701f7ce
PLSV <plsv297@gmail.com>
2023-10-13 16:23:43 +0530

    adding middleware for authorization and preventing unauthorized actions

--- api/main_test.go
+++ api/main_test.go
@@ -1,12 +1,15 @@
 package api
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/require"
 	db "pavanvasan.com/simplebank/db/sqlc"
+	"pavanvasan.com/simplebank/token"
 	"pavanvasan.com/simplebank/util"
 )
 
@@ -20,6 +23,18 @@ func newTestServer(t *testing.T, store db.Store) *Server {
 	require.NoError(t, err)
 
 	return server
+}
+
+func createAndSetAuthToken(t *testing.T, request *http.Request, tokenMaker token.Maker, username string) {
+	if len(username) == 0 {
+		return
+	}
+
+	token, err := tokenMaker.CreateToken(username, time.Minute)
+	require.NoError(t, err)
+
+	authorizationHeader := fmt.Sprintf("%s %s", authorizationTypeBearer, token)
+	request.Header.Set(authorizationHeaderKey, authorizationHeader)
 }
 
 func TestMain(m *testing.M) {
