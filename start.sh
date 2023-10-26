5ca9df02ea1c9561bad76775c96de208408920b4
PLSV <plsv297@gmail.com>
2023-10-27 00:56:14 +0530

    minor changes for testing

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ set -e
 
 echo "run db migration"
 source /app/app.env
+echo "--------------"
+echo $DB_SOURCE
+echo "--------------"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
