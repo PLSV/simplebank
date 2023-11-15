ff40ae46dc832b297e0db6231a38e3319d402d9b
PLSV <plsv297@gmail.com>
2023-11-15 13:50:36 +0530

    ensuring that we are skipping the email sending test

--- mail/sender_test.go
+++ mail/sender_test.go
@@ -2,28 +2,29 @@ package mail
 
 import (
 	"testing"
-	// "github.com/stretchr/testify/require"
-	// "pavanvasan.com/simplebank/util"
+
+	"github.com/stretchr/testify/require"
+	"pavanvasan.com/simplebank/util"
 )
 
 func TestSendEmailWithGmail(t *testing.T) {
-	// if testing.Short() {
-	// 	t.Skip()
-	// }
+	if testing.Short() {
+		t.Skip()
+	}
 
-	// config, err := util.LoadConfig("..")
-	// require.NoError(t, err)
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
 
-	// sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
 
-	// subject := "A test email"
-	// content := `
-	// <h1>Hello world</h1>
-	// <p>This is a test message from <a href="http://pavanvasan.com">Tech School</a></p>
-	// `
-	// to := []string{"plsv297@gmail.com"}
-	// attachFiles := []string{"../README.md"}
+	subject := "A test email"
+	content := `
+	<h1>Hello world</h1>
+	<p>This is a test message from <a href="http://pavanvasan.com">Tech School</a></p>
+	`
+	to := []string{"plsv297@gmail.com"}
+	attachFiles := []string{"../README.md"}
 
-	// err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
-	// require.NoError(t, err)
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
 }
